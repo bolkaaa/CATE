223cf3ff7411241e7e7c261cd2dc6e7db7d9a668
lrwz <lwyllie@airmail.cc>
2019-02-05 21:15:40 +0000

    Fixed CMakeLists for linux build

--- src/FileTree.hpp
+++ src/FileTree.hpp
@@ -2,6 +2,7 @@
 #define FILE_TREE_HPP
 
 #include <vector>
+#include <string>
 
 std::vector<std::string> get_subpaths(const std::string &root_path);
 void get_nested_files(std::vector<std::string> &paths, std::string root_path);
