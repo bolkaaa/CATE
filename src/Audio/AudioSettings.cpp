df3cad3127564035bd53e96e2cbfce7e460d033e
lrwz <lwyllie@airmail.cc>
2019-04-29 21:39:35 +0100

    fixed buffer size being set with default bin size in AudioSettings

--- src/Audio/AudioSettings.cpp
+++ src/Audio/AudioSettings.cpp
@@ -8,7 +8,7 @@ AudioSettings::AudioSettings()
           num_input_channels(2),
           num_output_channels(2)
 {
-    buffer_size = default_bin_size();
+    buffer_size = default_buffer_size();
     sample_rate = default_sample_rate();
     bin_size = default_bin_size();
 }
